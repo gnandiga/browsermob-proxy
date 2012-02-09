a1ab02fe173af0be670b4ac33b9d1b653f9111cb
Giridhar Nandigam <gnandiga@gmail.com>
2012-02-09 05:34:53 -0800

    Fixed sun import classes

--- src/test/java/org/browsermob/proxy/http/BrowserMobHttpClientTest.java
+++ src/test/java/org/browsermob/proxy/http/BrowserMobHttpClientTest.java
@@ -1,9 +1,5 @@
 package org.browsermob.proxy.http;
 
-import org.browsermob.proxy.http.BrowserMobHttpClient;
-import sun.jvm.hotspot.utilities.Assert;
-
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 public class BrowserMobHttpClientTest
@@ -19,6 +15,8 @@ public class BrowserMobHttpClientTest
 			String url = "http://cs.cruise.com/cs/default.aspx?type=left&logo=0&bestdeal=1&pc=n&skin=001&pin=%3C%=pin%%3E&did=-1&txt=224273&target=_top";
 
 			client.makeUri(url);
+
+			client.makeUri("http://cs.cruise.com/cs/default.aspx");
 		} catch (URISyntaxException e)
 		{
 			org.junit.Assert.fail("Exception was not supposed to happen here: " + e.getMessage());

4c08644267ac783ecfda4801a8ac84c5e39465ff
Giridhar Nandigam <gnandiga@gmail.com>
2012-02-09 06:31:17 -0800

    Chnaged from URI.getQuery to URI.getRawQuery as getQuery decodes as per the JVM and this converts certain characters into non-UTF-8 characters

--- src/test/java/org/browsermob/proxy/http/BrowserMobHttpClientTest.java
+++ src/test/java/org/browsermob/proxy/http/BrowserMobHttpClientTest.java
@@ -1,26 +1,31 @@
 package org.browsermob.proxy.http;
 
+import org.eclipse.jetty.util.MultiMap;
+import org.eclipse.jetty.util.UrlEncoded;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
 import java.net.URISyntaxException;
 
 public class BrowserMobHttpClientTest
 {
 
-	@org.junit.Test
+	@Test
 	public void verifyURI()
 	{
 
 		BrowserMobHttpClient client = new BrowserMobHttpClient();
 		try
 		{
-			String url = "http://cs.cruise.com/cs/default.aspx?type=left&logo=0&bestdeal=1&pc=n&skin=001&pin=%3C%=pin%%3E&did=-1&txt=224273&target=_top";
+			String url = "http://example.com/cs/default.aspx?type=left&logo=0&bestdeal=1&pc=n&skin=001&pin=%3C%=pin%%3E&did=-1&txt=224273&target=_top";
 
 			client.makeUri(url);
 
-			client.makeUri("http://cs.cruise.com/cs/default.aspx");
+			client.makeUri("http://example.com/cs/default.aspx");
 		} catch (URISyntaxException e)
 		{
-			org.junit.Assert.fail("Exception was not supposed to happen here: " + e.getMessage());
+			Assert.fail("Exception was not supposed to happen here: " + e.getMessage());
 		}
 	}
-
 }

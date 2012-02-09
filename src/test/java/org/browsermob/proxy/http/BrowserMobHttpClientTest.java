a3a115eb97dcfad3d17abd39d0d42b6c64271441
Giridhar Nandigam <gnandiga@gmail.com>
2012-02-09 05:24:53 -0800

    Added escape character functionality, so that double escapes dont come back with URISyntaxException

---
+++ src/test/java/org/browsermob/proxy/http/BrowserMobHttpClientTest.java
@@ -0,0 +1,15 @@
+package org.browsermob.proxy.http;
+
+import org.browsermob.proxy.http.BrowserMobHttpClient;
+
+public class BrowserMobHttpClientTest
+{
+
+	@org.junit.Test
+	public void verifyURI()
+	{
+
+		BrowserMobHttpClient client = new BrowserMobHttpClient();
+	}
+
+}
